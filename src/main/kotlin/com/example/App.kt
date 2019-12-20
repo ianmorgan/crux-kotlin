eb8fc64a472d7c84e02cd85b76c12c90bf648199
Ian Morgan <ian.morgan@r3.com>
2019-12-20 16:25:27 +0000

    updated readme with crux banner

--- src/main/kotlin/com/example/App.kt
+++ src/main/kotlin/com/example/App.kt
@@ -3,13 +3,29 @@ package com.example
 import clojure.lang.Keyword
 import crux.api.Crux
 
+
+val cruxTxPut = Keyword.intern("crux.tx/put")
+val cruxDbId = Keyword.intern("crux.db/id")
+val name = Keyword.intern("name")
+val lastName = Keyword.intern("last-name")
+
+
 fun main(args: Array<String>) {
     println("starting...")
 
-    val node = Crux.startNode(rocksDB())
+    val node = Crux.startNode(memDB())
     println(node)
 
     println(node.status())
+
+    // submit some docs
+    val id = "bart_simpson"
+    println("Submitting do with id $id")
+    val doc = mapOf (cruxDbId to Keyword.intern(id),
+            name to "Bart",
+            lastName to "Simpson")
+    val txResult = node.submitTx(listOf(listOf(cruxTxPut, doc)))
+    println(txResult)
 }
 
 
@@ -23,7 +39,7 @@ fun rocksDB(): Map<Keyword, Any> {
 
 fun memDB(): Map<Keyword, Any> {
     return mapOf(Keyword.intern("crux.node/topology") to Keyword.intern("crux.standalone/topology"),
-            Keyword.intern("crux.node/kv-store") to "crux.kv.mendb/kv",
+            Keyword.intern("crux.node/kv-store") to "crux.kv.memdb/kv",
             Keyword.intern("crux.kv/db-dir") to "data/db-dir-1",
             Keyword.intern("crux.standalone/event-log-dir") to "data/eventlog-1",
             Keyword.intern("crux.standalone/event-log-kv-store") to "crux.kv.memdb/kv"
